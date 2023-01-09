9a5fa841ab0efed170ae4f30ecc43a0778de1315
Kacper Pawlak <Kacper.Pawlak@365.aplitt.pl>
2023-01-09 16:56:56 +0100

    Dodaj pliki projektów.

---
+++ lib1/DatabaseManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lib1
+{
+    public class DatabaseManager
+    {
+        private SqlConnection connection;
+
+        public DatabaseManager(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        public void AddBook(string title, string author, string year)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand($"INSERT INTO books (Title, Author, Year) VALUES ('{title}', '{author}', {year})", connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public void EditBook(int bookId, string title, string author, string year)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE books SET Title = @title, Author = @author, Year = @year WHERE Id = @bookId", connection);
+            command.Parameters.AddWithValue("@bookId", bookId);
+            command.Parameters.AddWithValue("@title", title);
+            command.Parameters.AddWithValue("@author", author);
+            command.Parameters.AddWithValue("@year", year);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM books WHERE Id = @bookId", connection);
+            command.Parameters.AddWithValue("@bookId", bookId);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public List<Book> getbooks() 
+        {
+
+            List<Book> books = new List<Book>();
+
+            String query = "SELECT * FROM books";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            SqlDataReader dbr;
+            try
+            {
+                connection.Open();
+                dbr = cmd.ExecuteReader();
+                while (dbr.Read())
+                {
+                    string sID = dbr["Id"].ToString();
+                    string stitle = (string)dbr["Title"]; // name is string value
+                    string sauthor = (string)dbr["Author"];
+                    string syear = (string)dbr["Year"];
+                    Book book = new Book()
+                    {
+                        Id = int.Parse(sID),
+                        Title = stitle,
+                        Author = sauthor,
+                        Year = syear,
+                    };
+                    books.Add(book);
+                }
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+
+            connection.Close();
+
+            return books;
+        }
+    }
+}
---
+++ lib1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lib1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        public DatabaseManager dbManager = new DatabaseManager("Server=localhost;Database=library;Trusted_Connection=True;");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void deleteBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (bookDataGrid.SelectedItem != null)
+            {
+                Book? passedBook;
+                if (bookDataGrid.SelectedItem != null)
+                {
+                    passedBook = bookDataGrid.SelectedItem as Book;
+                }
+                else
+                {
+                    passedBook = new Book();
+                }
+                dbManager.DeleteBook(passedBook.Id);
+                czystka();
+            }
+        }
+
+        private void editBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (bookDataGrid.SelectedItem != null)
+            {
+                Book? passedBook;
+                if (bookDataGrid.SelectedItem != null)
+                {
+                    passedBook = bookDataGrid.SelectedItem as Book;
+                }
+                else
+                {
+                    passedBook = new Book();
+                }
+                dbManager.EditBook(passedBook.Id, titlebox.Text, authorbox.Text, yearbox.Text);
+                czystka();
+            }
+            
+        }
+
+        private void addBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            dbManager.AddBook(titlebox.Text, authorbox.Text, yearbox.Text);
+        }
+
+        private void showbutton_Click(object sender, RoutedEventArgs e)
+        {
+            bookDataGrid.ItemsSource= dbManager.getbooks();
+        }
+
+        private void czystka() 
+        {
+            titlebox.Clear();
+            authorbox.Clear();
+            yearbox.Clear();
+        }
+    }
+}
